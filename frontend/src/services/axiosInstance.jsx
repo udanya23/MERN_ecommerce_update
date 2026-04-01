373664a5b107119476012be25c0da48b406a069f
udanya23 <udanya2001@gmail.com>
2026-04-01 15:19:06 +0530

    url changed

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL:"http://localhost:2000/api",
+    baseURL:`${import.meta.env.VITE_API_URL}`,
     withCredentials:true
 })
 instance.interceptors.request.use((config)=>{
