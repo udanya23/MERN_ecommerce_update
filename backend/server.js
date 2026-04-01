50407951abded125d11cbe688c080c6ba8c57760
udanya23 <udanya2001@gmail.com>
2026-04-01 16:25:33 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ require("dotenv").config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://mern-ecommerce-frontend-04p7.onrender.com",
     credentials:true
 }))
 connectDB(app)
